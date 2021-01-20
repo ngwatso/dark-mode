fc58895458262f4291d5fabdf18e3a29dfe9e0fd
ngwatso <ngwatson79@yahoo.com>
2021-01-20 11:13:25 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,23 +8,22 @@ import Navbar from "./components/Navbar";
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+	const [coinData, setCoinData] = useState([]);
+	const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Charts coinData={coinData} />
-    </div>
-  );
+	useEffect(() => {
+		axios.get(
+			"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+		)
+			.then((res) => setCoinData(res.data))
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<div className={darkMode ? "dark-mode App" : "App"}>
+			<Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+			<Charts coinData={coinData} />
+		</div>
+	);
 };
 
 const rootElement = document.getElementById("root");
